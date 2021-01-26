18582ce168423dfff569ec07060fc057e65da57f
Daniel Ahn <iamdanahn@gmail.com>
2021-01-25 21:17:38 -0500

    add clickevent listeners to gmaps

--- frontend/components/map/map.jsx
+++ frontend/components/map/map.jsx
@@ -1,50 +1,91 @@
 import React from 'react';
 
+
+const getCoordsObj = (latLng) => ({
+	lat: latLng.lat(),
+	lng: latLng.lng(),
+});
+
+const mapProps = {
+  zoom: 14,
+  center: new google.maps.LatLng(37.7758, -122.435),
+  mapTypeId: "roadmap",
+};
+
 class Map extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.initMap = this.initMap.bind(this);
-  }
+		this.state = {
+			miles: 0,
+		};
 
-  componentDidMount() {
-    this.initMap();
-  }
+		this.initMap = this.initMap.bind(this);
+	}
 
-  usersPosition(map) {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition( (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-        map.setCenter(pos);
-      });
-    }
-  }
+	componentDidMount() {
+		this.initMap();
+	}
 
-  initMap() {
-    const mapProps = {
-      zoom: 14,
-      center: new google.maps.LatLng(37.7758, -122.435),
-      mapTypeId: "roadmap",
-    };
-    
+	initMap() {
+		const map = new google.maps.Map(this.mapNode, mapProps);
+		this.usersPosition(map);
+	}
 
-    const map = new google.maps.Map(this.mapNode, mapProps);
-    this.usersPosition(map);
-    
-  }
+	// obtains user's current position if allowed
+	usersPosition(map) {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition((position) => {
+				const pos = {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude,
+				};
+				map.setCenter(pos);
+			});
+		}
+	}
 
-  render() {
-    return(
-      <div id="map-container" ref={map => this.mapNode = map}> 
-        Map
-      </div>
-    )
+	addMarker(location, map) {
+		const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		let labelIndex = 0;
 
-  }
+		new google.maps.Marker({
+			position: location,
+			label: labels[labelIndex++ % labels.length],
+			map: map,
+		});
+	}
 
+	eventListeners() {
+		google.maps.event.addListener(this.map, "idle", () => {
+			const { north, south, east, west } = this.map.getBounds().toJSON();
+			const bounds = {
+				northEast: { lat: north, lng: east },
+				southWest: { lat: south, lng: west },
+			};
+			// this.props.updateFilter("bounds", bounds);
+		});
+		google.maps.event.addListener(this.map, "click", (event) => {
+			const coords = getCoordsObj(event.latLng);
+      this.handleClick(coords);
+      console.log(coords)
+		});
+	}
+
+  handleClick(coords) {
+		this.props.history.push({
+			pathname: "routes/create",
+			search: `lat=${coords.lat}&lng=${coords.lng}`,
+		});
+	}
+
+	render() {
+		return (
+			<div id="map-container" ref={(map) => (this.mapNode = map)}>
+				Map
+			</div>
+		);
+	}
 }
 
 export default Map;
--- frontend/components/routes/create_route_form.jsx
+++ frontend/components/routes/create_route_form.jsx
@@ -5,46 +5,55 @@ class CreateRouteForm extends React.Component {
     super(props)
     this.state = {
       route_title: "",
-  
+      search: ""
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  update(field) {
+    return e => {
+    this.setState({ [field]: e.currentTarget.value})
+    }
+  }
+
 
   handleSubmit(e){
     e.preventDefault();
 
   }
 
-
   render () {
     return (
-      <div className="create-route">
-        <div className="cr-search-bar">
-          <input
-            type="text"
-            placeholder="Enter location"
-          />
-          <button onClick={this.handleSubmit}>
-            Search
-          </button>
-        </div>
+			<div className="create-route">
+				<div className="cr-search-bar">
+					<input
+						type="text"
+						placeholder="Enter location"
+						value={this.state.search}
+						onChange={this.update("search")}
+					/>
+					<button onClick={this.handleSubmit}>Search</button>
+				</div>
 
-        <div>
-          <h3>Route Details</h3>
-          <input
-            type="text"
-            value={this.state.route_title}  
-          />
-          <select>
-            <option>Choose an Activity</option>
-            <option>Walk</option>
-            <option>Run</option>
-            <option>Bike</option>
-          </select>
-        </div>
-      </div>
-    )
+				<div>
+					<h3>Route Details</h3>
+					<input
+						type="text"
+						value={this.state.route_title}
+						onChange={this.update("route_title")}
+            placeholder="Route title"
+					/>
+
+					<select>
+						<option>Choose an Activity</option>
+						<option>Walk</option>
+						<option>Run</option>
+						<option>Bike</option>
+					</select>
+				</div>
+			</div>
+		);
   }
 }
 
