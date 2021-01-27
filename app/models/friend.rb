238a7dffb8cf92adfc1cfaf327793887991581b5
Daniel Ahn <iamdanahn@gmail.com>
2021-01-26 20:14:59 -0500

    Correct Rails Models

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -12,8 +12,8 @@ class Friend < ApplicationRecord
   validates :user_id, :friend_id, presence: true
 
   belongs_to :user,
-    primary_key :id,
-    foreign_key :user_id,
+    primary_key: :id,
+    foreign_key: :user_id,
     class_name: :User
 
 end
