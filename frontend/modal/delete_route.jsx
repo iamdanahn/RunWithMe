8ae05f5c11bcf8742ef2508df316b1dec680f62f
Daniel Ahn <iamdanahn@gmail.com>
2021-02-23 10:35:44 -0500

    Deprecated delete route files, merged with modal file

--- frontend/modal/delete_route.jsx
+++ frontend/modal/delete_route.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+// Deprecated, holding for review
 
 
-const DeleteRoute = ({route, deleteRoute, closeModal}) => {
-   debugger
-  return (
-    <div className="modal-delete">
-      <h3>Delete Route?</h3>
-      <div className="modal-btns">
-        <button className="modal-btns-ok" onClick={() => deleteRoute()}>
-          OK
-        </button>
-        <button className="modal-btns-cancel" onClick={closeModal}>
-          CANCEL
-        </button>
-      </div>
-    </div>
-  )
-}
+// import React from 'react';
+// import { Link } from 'react-router-dom'
 
-export default DeleteRoute;
+
+// const DeleteRoute = ({route, deleteRoute, closeModal}) => {
+//    debugger
+//   return (
+//     <div className="modal-delete">
+//       <h3>Delete Route?</h3>
+//       <div className="modal-btns">
+//         <button className="modal-btns-ok" onClick={() => deleteRoute()}>
+//           OK
+//         </button>
+//         <button className="modal-btns-cancel" onClick={closeModal}>
+//           CANCEL
+//         </button>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default DeleteRoute;
--- frontend/modal/delete_route_container.js
+++ frontend/modal/delete_route_container.js
@@ -1,21 +1,23 @@
-import { connect } from 'react-redux';
-import { deleteRoute } from '../actions/route_actions'
-import { closeModal } from '../actions/modal_actions'
-import DeleteRoute from './delete_route'
+// Deprecated, holding for review
 
-const msp = (state, ownProps) => {
-  debugger
-  // ownProps does not have a routeId
-  return {
-    route: ownProps.routeid,
-  }
-}
+// import { connect } from 'react-redux';
+// import { deleteRoute } from '../actions/route_actions'
+// import { closeModal } from '../actions/modal_actions'
+// import DeleteRoute from './delete_route'
 
-const mdp = dispatch => {
-  return ({
-    // deleteRoute: (routeId) => dispatch(deleteRoute(routeId)),
-    closeModal: () => dispatch(closeModal())
-  })
-}
+// const msp = (state, ownProps) => {
+//   debugger
+//   // ownProps does not have a routeId
+//   return {
+//     route: ownProps.routeid,
+//   }
+// }
 
-export default connect(msp, mdp)(DeleteRoute);
+// const mdp = dispatch => {
+//   return ({
+//     // deleteRoute: (routeId) => dispatch(deleteRoute(routeId)),
+//     closeModal: () => dispatch(closeModal())
+//   })
+// }
+
+// export default connect(msp, mdp)(DeleteRoute);
