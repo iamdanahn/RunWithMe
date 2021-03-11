c5dd7b837b9be0cb8dda66463a7ad08c100a3229
Daniel Ahn <iamdanahn@gmail.com>
2021-03-11 17:54:16 -0500

    fixd route show fetchUser process

--- frontend/components/routes/route_show.jsx
+++ frontend/components/routes/route_show.jsx
@@ -20,7 +20,7 @@ class RouteShow extends React.Component {
 		// 	.fetchRoute(routeId)
 		// 	.then(() => this.props.fetchComments(routeId))
 		// 	.then(() => this.initMap());
-		this.props.fetchUser(this.props.match.params.routeId);
+		this.props.fetchUser(this.props.routes[routeId].creator_id);
 
 		if (!this.props.routes[routeId]) {
 			this.props.fetchRoute(routeId).then(() => this.initMap());
@@ -35,7 +35,7 @@ class RouteShow extends React.Component {
 			// this.props.fetchComments(this.props.match.params.routeId);
 			this.props
 				.fetchRoute(this.props.match.params.routeId)
-				.then(() => this.props.fetchUser(this.props.match.params.routeId))
+				.then(() => this.props.fetchUser(this.props.routes[routeId]).creator_id)
 				.then(() => this.initMap());
 		}
 	}
