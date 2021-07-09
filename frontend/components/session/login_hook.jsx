aaa933171c1d157b7a80d53d6a69da3c7f5e8613
Daniel Ahn <iamdanahn@gmail.com>
2021-07-09 17:05:40 -0400

    stuck on onChange handler to take multiple inputs while converting to hooks

--- frontend/components/session/login_hook.jsx
+++ frontend/components/session/login_hook.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const Login = () => {
+export default function Login({ clearErrors }) {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-};
 
-export default Login;
+	useEffect(() => {
+		//used only for unmounting
+
+		return () => {
+			const errors = [];
+			clearErrors(errors);
+		};
+	});
+
+	function update(field) {
+		return (e) => {
+			e.target.className = "input";
+		};
+	}
+}
