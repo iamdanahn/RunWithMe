2812a82fb94c02e33d53d0ee8340cf003d24d365
Daniel Ahn <iamdanahn@gmail.com>
2021-07-09 18:11:59 -0400

    login hook component not rendering errors... not sure why

--- frontend/components/session/login_hook.jsx
+++ frontend/components/session/login_hook.jsx
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 
 export default function Login({ clearErrors, errors, login, loginDemo }) {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+	const [values, setValues] = useState({
+		email: "",
+		password: "",
+	});
+
+  const {email, password } = values;
   const history = useHistory();
   const errPresent = errors.length > 0;
 
@@ -20,28 +24,79 @@ export default function Login({ clearErrors, errors, login, loginDemo }) {
 		return (e) => {
 			e.target.className = "input";
 
-			switch (field) {
-				case "email":
-					setEmail(e.target.value);
-				case "password":
-					setPassword(e.target.value);
-			}
+      const newValues = { ...values, [e.target.name]: e.target.value };
+      setValues(newValues);      
 		};
 	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		const user = { email, password };
-    login(user).then(() => history.push("/dashboard"));
+		login(user).then(() => history.push("/dashboard"));
 	};
 
-  const handleDemoLogin = (e) => {
-    e.preventDefault();
-    loginDemo().then(() => history.push("/dashboard"))
-  }
+	const handleDemoLogin = (e) => {
+		e.preventDefault();
+		loginDemo().then(() => history.push("/dashboard"));
+	};
 
-  
+	return (
+		<section className="auth-form-ctr">
+			<div className="auth-form">
+				<div className="header">
+					<Link className="other-link" to="/signup">
+						<span>SIGN UP</span>
+					</Link>
+					<br />
+				</div>
+				<button id="btn-demo" onClick={handleDemoLogin}>
+					DEMO LOGIN
+				</button>
 
+				<div className="or-box">
+					<span className="or-box line"></span>
+					<span className="or-box or"> OR </span>
+					<span className="or-box line"></span>
+				</div>
+
+				<form className="auth-form input-area">
+					<div>
+						<input
+							className={errPresent ? "input err-border" : "input login"}
+							type="email"
+							name="email"
+							value={email}
+							onChange={update("email")}
+							placeholder="Email from hook"
+						/>
+					</div>
+
+					<br />
+
+					<div>
+						<input
+							className={errPresent ? "input err-border" : "input login"}
+							type="password"
+							name="password"
+							value={password}
+							onChange={update("password")}
+							placeholder="Password"
+						/>
+						<div></div>
+					</div>
+					<br />
+
+					<div className="auth-form errors"> {errors} </div>
+
+					<br />
+
+					<button className="auth-form btn" onClick={handleSubmit}>
+						LOGIN
+					</button>
+				</form>
+			</div>
+		</section>
+	);
 }
 
   // METHOD 2 - onChange
@@ -50,7 +105,11 @@ export default function Login({ clearErrors, errors, login, loginDemo }) {
   //   password: ""
 	// });
 
-	// const onChangeFunc = (e) => {
-	// 	const newValues = { ...values, [e.target.name]: e.target.value };
-	// 	setValues(newValues);
-	// };
+			// switch (field) {
+			// 	case "email":
+			// 		setEmail(e.target.value);
+			// 		break;
+			// 	case "password":
+			// 		setPassword(e.target.value);
+			// 		break;
+			// }
